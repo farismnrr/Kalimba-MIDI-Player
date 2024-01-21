594921820bcf889ec7e4311d71930c42bd38c988
Faris Munir <120432426+farismnrr@users.noreply.github.com>
2024-01-22 02:47:05 +0700

    Update install_and_run.sh

--- install_and_run.sh
+++ install_and_run.sh
@@ -7,7 +7,7 @@ sudo apt-get install python3-pip
 python -m pip install --upgrade pip
 
 # Updating and installing required modules...
-pip install --upgrade mido midi keyboard pyautogui argparse
+pip install --upgrade mido keyboard pyautogui argparse
 
 # Installation and update complete. Running kalimba_player...
 python kalimba_player.py
