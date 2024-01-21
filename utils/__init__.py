132339a4eeb2795b8074921f530efe94889f4adc
Faris Munir <120432426+farismnrr@users.noreply.github.com>
2024-01-21 19:21:09 +0000

    delete previous version

--- utils/__init__.py
+++
@@ -1,3 +0,0 @@
-
-from .shift import find_best_shift
-from .midi_filter import midi_play_filter
--- utils/midi_filter.py
+++
@@ -1,8 +0,0 @@
-
-def midi_play_filter(msg, channels):
-    if msg.is_meta or msg.type != 'note_on':
-        return False
-    if channels and not msg.channel in channels:
-        return False
-    return True
-
