44d5657bd2fddadaf34102ac1f6c7f281867abdd
Faris Munir <120432426+farismnrr@users.noreply.github.com>
2024-01-23 14:47:07 +0700

    Update install_and_run.bat

--- install_and_run.bat
+++ install_and_run.bat
@@ -1,4 +1,9 @@
 @echo off
+echo Automatic run as admin...
+%1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
+set CURRENT_PATH=/d %~dp0
+cd %CURRENT_PATH%
+
 echo Updating pip...
 python.exe -m pip install --upgrade pip
 
